897c7cee4656fad191a95b11befb06a631864946
Ellen Shimada <ellenshimada@gmail.com>
2019-07-17 20:53:27 -0700

    Test activity for Tasks

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -27,4 +27,24 @@ class ActivityFeedTest extends TestCase
         $project->update(['title' => 'Changed']);
         $this->assertEquals('updated', $project->activity->last()->description);
     }
+
+    /** @test **/
+    public function creating_a_new_task_records_project_activity()
+    {
+        $project = ProjectFactory::create();
+        $project->addTask('Do something');
+        $this->assertEquals('created_task', $project->activity->last()->description);
+    }
+
+    /** @test **/
+    public function completing_a_task_records_activity()
+    {
+        $project = ProjectFactory::withTasks(1)->create();
+        $this->actingAs($project->owner)->patch($project->tasks[0]->path(),[
+            'body' => 'foobar',
+            'completed' => true
+        ]);
+        $this->assertEquals('completed_task', $project->activity->last()->description);
+    }
+
 }
