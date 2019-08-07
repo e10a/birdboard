03b5e8b3085dbbcb2aa686e30333524d67ae4cd9
Ellen Shimada <ellenshimada@gmail.com>
2019-08-06 19:50:01 -0700

    Form requests and more tests for invitations

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -13,7 +13,42 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test **/
-    public function a_project_can_invite_a_user()
+    function non_owners_may_not_invite_users()
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+    }
+    /** @test **/
+    public function a_project_owner_can_invite_a_user()
+    {
+        $this->withoutExceptionHandling();
+        $project = ProjectFactory::create();
+        $userToInvite = factory(User::class)->create();
+        $this->actingAs($project->owner)->post(
+            $project->path() . '/invitations', [
+                'email' => $userToInvite->email
+            ]
+        )
+        ->assertRedirect($project->path());
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test **/
+    function invite_email_must_belong_to_an_existing_user()
+    {
+        $project = ProjectFactory::create();
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => 'notauser@example.com'
+            ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have an account'
+            ]);
+    }
+
+    /** @test **/
+    public function invited_users_may_update_project_details()
     {
         $project = ProjectFactory::create();
         $project->invite($newUser = factory(User::class)->create());
